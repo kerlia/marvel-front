b2d5491c95ca93972601beb66ca68302a778431d
Guillaume B <guillaume@kura.fr>
2020-02-20 10:01:02 +0100

    Set css link for character listing

--- src/components/CharacterItem.js
+++ src/components/CharacterItem.js
@@ -3,13 +3,15 @@ import React from "react";
 function CharacterItem({ name, thumbnail, description }) {
   return (
     <li>
-      <div className="ima">
-        <img src={`${thumbnail.path}.${thumbnail.extension}`} alt={name} />
-      </div>
-      <div className="info">
-        <h2>{name}</h2>
-        <p>{description}</p>
-      </div>
+      <a href="#">
+        <div className="ima">
+          <img src={`${thumbnail.path}.${thumbnail.extension}`} alt={name} />
+        </div>
+        <div className="info">
+          <h2>{name}</h2>
+          <p>{description}</p>
+        </div>
+      </a>
     </li>
   );
 }
